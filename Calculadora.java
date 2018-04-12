710fbac3421d386d8426b163c671b74e2477b297
kurapika112 <35772574+kurapika112@users.noreply.github.com>
2018-04-12 16:28:03 +0200

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,31 @@
+package m5.uf3.gloria;
+
+public class Calculadora {
+
+	private int num1;
+	private int num2;
+
+	public Calculadora(int a, int b) {
+		this.num1 = a;
+		this.num2 = b;
+	}
+
+	public int divideix() {
+		int resul = num1 / num2;
+		return resul;
+	}
+
+	public boolean residu() {
+
+		if (num1 % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public int potencia() {
+		
+		return num1^num2;
+	}
+}
---
+++ CalculadoraExamenTest.java
@@ -0,0 +1,34 @@
+package m5.uf3.gloria;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class CalculadoraExamenTest {
+
+	@Test
+	void testPotencia() {
+		Calculadora calc = new Calculadora(3, 2);
+		assertTrue(calc.residu());
+	}
+	
+	@Test
+	void testResidu1() {
+		Calculadora calc = new Calculadora(3, 2);
+		assertFalse(calc.residu());
+	}
+	
+	@Test
+	void testResidu2() {
+		Calculadora calc = new Calculadora(2, 2);
+		assertTrue(calc.residu());
+	}
+	
+	
+
+}
